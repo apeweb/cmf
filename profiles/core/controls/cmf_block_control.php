b0857a58bb994d92e1fb821c27ee54252167caaa
Matthew Bonner <matthew.bonner@gmail.com>
2012-05-17 13:29:00 +0100

    New cmf block control

---
+++ profiles/core/controls/cmf_block_control.php
@@ -0,0 +1,48 @@
+<?php
+
+if (count(debug_backtrace()) == 0) {
+  header('Content-Type: text/plain; charset=UTF-8', TRUE, 403);
+  die("The page cannot be displayed.\r\nThe request has not been fulfilled because the server does not authorise access to this request externally.");
+}
+ 
+class Cmf_Block_Control extends Cmf_Control {
+  public $cssClass = 'block';
+
+  public static function render (Cmf_Form_Control $control) {
+    $control->_content = '<div';
+
+    // core attibutes
+    if (trim($control->cssClass) != '') {
+      $control->_content .= ' class="' . $control->cssClass . '"';
+    }
+    if (trim($control->id) != '') {
+      $control->_content .= ' id="' . $control->id . '"';
+    }
+    if (trim($control->style) != '') {
+      $control->_content .= ' style="' . $control->style . '"';
+    }
+    if (trim($control->toolTip) != '') {
+      $control->_content .= ' title="' . $control->toolTip . '"';
+    }
+
+    // i18n attributes
+    if (trim($control->lang) != '') {
+      $control->_content .= ' lang="' . $control->lang . '" xml:lang="' . $control->lang . '"';
+    }
+    if (trim($control->direction) != '') {
+      $control->_content .= ' dir="' . $control->direction . '"';
+    }
+
+    $control->_content .= ">";
+
+    // Children
+    foreach ($control->children as $child) {
+      $child->process();
+      $control->_content .= $child->getContent();
+    }
+
+    $control->_content .= '</div>';
+  }
+}
+
+?>
