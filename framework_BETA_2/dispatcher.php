f5559aedda237fa8c14a43ee4172ca5d583e05c1
Matthew Bonner <matthew.bonner@gmail.com>
2012-05-24 23:22:24 +0100

    Dispatch renamed to Dispatcher

--- framework_BETA_2/dispatcher.php
+++ framework_BETA_2/dispatcher.php
@@ -5,11 +5,6 @@ if (count(debug_backtrace()) == 0) {
   die("The page cannot be displayed.\r\nThe request has not been fulfilled because the server does not authorise access to this request externally.");
 }
 
-// xxx need to look into what dispatcher does
-
-// does a request, as if somebody had typed the address in the browser
-$dispatcher = new Request_Dispatcher;
-$dispatcher->startPoint = new Request('/contact-us/map/');
-$dispatcher->getRoute();
+// xxx need to create
 
 ?>
