57019c87c44fc911833fae4b6c2e8e60487c3042
Matthew Bonner <matthew.bonner@gmail.com>
2012-05-24 22:35:31 +0100

    The finished activator class

--- framework_BETA_2/activator.php
+++ framework_BETA_2/activator.php
@@ -5,36 +5,27 @@ if (count(debug_backtrace()) == 0) {
   die("The page cannot be displayed.\r\nThe request has not been fulfilled because the server does not authorise access to this request externally.");
 }
 
-/***************************
+class Activator implements iController_Factory {
+  protected $_controllerInstances = array();
 
-DO NOT DELETE, PART OF THE MVC THAT WILL BE WORKED ON
+  public function createController ($controller)  {
+    Assert::isString($controller);
 
-***********************/
+    if (Validator::isValidPhpIdentifier($controller) == FALSE) {
+      throw new Argument_Exception("Class '{$controller}' is not a valid class name");
+    }
 
+    if (class_exists($controller) == FALSE) {
+      throw new RuntimeException("Class '{$controller}' could not be found");
+    }
 
+    if (isset($this->_controllerInstances[$controller]) == TRUE) {
+      return $this->_controllerInstances[$controller];
+    }
 
+    $this->_controllerInstances[$controller] = new $controller;
 
-// needs to find files
-
-// see http://code.google.com/p/kohana-mptt/source/browse/trunk/Dispatch.php
-// and http://forum.kohanaframework.org/discussion/88/how-do-you-call-a-controller-method-in-a-view/p1
-
-// dispatch is the wrong word used, should be activator
-//$article_list=Activator::controller('article')->method('list',5);
-//$article_list=Activator::controller('article')->method('list',array(5,'last');
-
-/**
- * Syntax should be 
- * $articleList = Dispatch::controller('article');
- * $articleList->list(5, 'last');
- */
-
-// needs to find files
-
-class Activator {
-  static public function createInstance ($controller, $action) {
-    echo $controller, '/' . $action;
-    exit;
+    return $this->_controllerInstances[$controller];
   }
 }
 
