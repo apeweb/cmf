22e7db0b82e7489a0f56a579f28b7979c1fe26f1
Matthew Bonner <matthew.bonner@gmail.com>
2012-05-24 23:29:22 +0100

    New abstract CMF Module class

---
+++ framework_BETA_2/cmf_module.php
@@ -0,0 +1,14 @@
+<?php
+
+if (count(debug_backtrace()) == 0) {
+  header('Content-Type: text/plain; charset=UTF-8', TRUE, 403);
+  die("The page cannot be displayed.\r\nThe request has not been fulfilled because the server does not authorise access to this request externally.");
+}
+
+abstract class Cmf_Module {
+  abstract static public function install();
+  abstract static public function uninstall();
+  abstract static public function initialise();
+}
+
+?>
