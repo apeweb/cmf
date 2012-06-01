c7c8be405ad8e8f5d2456509f2e415ba3eeb9985
Matthew Bonner <matthew.bonner@gmail.com>
2012-06-01 11:59:40 +0100

    Added a shorthand for getting view data to make views clearer

--- framework_BETA_2/view_data.php
+++ framework_BETA_2/view_data.php
@@ -5,6 +5,11 @@ if (count(debug_backtrace()) == 0) {
   die("The page cannot be displayed.\r\nThe request has not been fulfilled because the server does not authorise access to this request externally.");
 }
 
+// shorthand for views
+function View_Data ($key) {
+  return View_Data::getValue($key);
+}
+
 class View_Data extends Memory {
   public static function getValue ($key) {
     try {
